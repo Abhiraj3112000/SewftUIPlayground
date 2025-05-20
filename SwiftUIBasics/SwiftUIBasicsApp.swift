95a783976ed543792524b3f82f3450a79dea4084
abhirajchatterjee <chatterjeeabhirajofficial@gmail.com>
2025-05-20 20:18:23 +0530

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Abhiraj Chatterjee on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
