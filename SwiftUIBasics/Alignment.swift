a1a4c1da2376de32daa2688b325362445a6b0d07
Abhiraj Chatterjee <64791997+Abhiraj3112000@users.noreply.github.com>
2025-06-10 23:19:53 +0530

    frames

---
+++ SwiftUIBasics/Alignment.swift
@@ -0,0 +1,30 @@
+//
+//  Alignment.swift
+//  SwiftUIBasics
+//
+//  Created by Abhiraj Chatterjee on 10/06/25.
+//
+
+import SwiftUI
+
+struct Alignment: View {
+    var body: some View {
+        Text("Hello, World!")
+            .background(Color.red)
+            .frame(height: 100, alignment: .top)
+            .background(Color.orange)
+            .frame(width: 150)
+            .background(Color.purple)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.pink)
+            .frame(height: 400)
+            .background(Color.green)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(Color.yellow)
+        
+    }
+}
+
+#Preview {
+    Alignment()
+}
