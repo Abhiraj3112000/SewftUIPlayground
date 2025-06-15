335066fb687eb8f655c80401dcf879f6d8e02cc1
Abhiraj Chatterjee <64791997+Abhiraj3112000@users.noreply.github.com>
2025-06-16 01:56:59 +0530

    instagram gradient experiment

--- SwiftUIBasics/Shapes.swift
+++ SwiftUIBasics/Shapes.swift
@@ -22,11 +22,22 @@ struct Shapes: View {
         Image("ethan")
             .resizable()
             .scaledToFit()
-            .frame(width: 300, height: 300)
+            .frame(width: 100, height: 100)
             .clipShape(Circle())
             .padding(8)
+            .frame(width: 110, height: 110)
+            .background(
+                //Color.red
+                LinearGradient(
+                    gradient: Gradient(colors: [Color(#colorLiteral(red: 0.7181442055, green: 0.08581728649, blue: 0.6490891237, alpha: 1)), Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)), Color(#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)), Color(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1))]),
+                    startPoint: .topTrailing,
+                    endPoint: .bottomLeading)
+            )
+            .clipShape(Circle())
         Text("Ethan Mascarenhas")
-            .font(.system(size: 25))
+            .font(.system(size: 15, design: .serif))
+            .frame(width: 100)
+            .multilineTextAlignment(.center)
 //        Rectangle()
 //            .trim(from: 0.0, to: 0.6)
 //            .frame(width:400,height:200,alignment: Alignment.center)
